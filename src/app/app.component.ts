e375cc47170aee162fecc88bf536863ee712aed1
Bastian Wagner <bastian.wagner@softconcis.de>
2021-09-01 09:17:09 +0200

    settings done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '@src/environments/environment';
 import { test } from '@app/test';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app-architecture';
+export class AppComponent implements OnInit {
+  title = 'app-architecture-';
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.afs.collection('test').snapshotChanges().subscribe(items => {
+      console.log(items.map(x => x.payload.doc.data()));
+    });
+  }
 }
