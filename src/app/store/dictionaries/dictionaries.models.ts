0abd5618635cb9053b1240d7e983116568531ef2
Bastian Wagner <bastian.wagner@softconcis.de>
2021-09-02 19:11:35 +0200

    rgnx store

---
+++ src/app/store/dictionaries/dictionaries.models.ts
@@ -0,0 +1,14 @@
+import { ControlItem, Item } from "@app/models/frontend";
+export { ControlItem, Item } from "@app/models/frontend";
+
+export interface Dictionaries {
+    roles: Dictionary;
+    specializations: Dictionary;
+    qualifications: Dictionary;
+    skills: Dictionary;
+}
+
+export interface Dictionary {
+    items: Item[];
+    controlItems: ControlItem[];
+}
